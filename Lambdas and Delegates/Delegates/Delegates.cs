73046a6ec1fb13d0aab44d1edd49226d3dd7122e
Harry Keys <harry.keys40@gmail.com>
2025-07-24 19:08:06 +1000

    Created delegate example

---
+++ Lambdas and Delegates/Delegates/Delegates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class Delegates
+    {
+        public delegate void Int32Printer(int x);
+        public delegate void Int64Printer(long x);
+
+        public static void PrintInt32(Int32Printer print)
+        {
+            int x = 5;
+            print(x);
+        }
+
+        public static void PrintInt64(Int64Printer print)
+        {
+            long x = 5L;
+            print(x);
+        }
+    }
+}
--- Lambdas and Delegates/Delegates/Program.cs
+++ Lambdas and Delegates/Delegates/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using static Delegates.Delegates;
+
+//Getting used familiar with the syntax of custom delegates
+Int32Printer int32Printer = new Int32Printer((x) => Console.WriteLine(x));
+Int64Printer int64Printer = new Int64Printer((x) => Console.WriteLine(x));
+
+Delegates.Delegates.PrintInt32(int32Printer);
+Delegates.Delegates.PrintInt64(int64Printer);
