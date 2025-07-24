af578f839ae9dd8bbb8936105b86dd81dc515507
Harry Keys <harry.keys40@gmail.com>
2025-07-24 19:00:23 +1000

    Expression tree examples

--- Lambdas and Delegates/Lambdas/Program.cs
+++ Lambdas and Delegates/Lambdas/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 using Lambdas;
+
+//Capturing variables in lambda expressions
+Console.WriteLine("##### Capturing variables in lambda expressions #####");
 
 var methodParameter = "This is a method parameter";
 var lambdaParameter = "This is a lambda parameter";
@@ -24,3 +26,22 @@ counterLambda[0].Invoke();
 counterLambda[0].Invoke();
 counterLambda[1].Invoke();
 counterLambda[1].Invoke();
+
+Console.WriteLine("###################################");
+//
+
+//Expression Trees
+Console.WriteLine("##### Expression Trees #####");
+
+var expressionTrees = new ExpressionTrees();
+
+var expression = expressionTrees.CreateExpression();
+Console.WriteLine(expression);
+
+expressionTrees.InspectExpression();
+expressionTrees.GetProducts("Desk Chair", "Furniture", 200);
+expressionTrees.GetProducts(null, "Stationery", 10);
+expressionTrees.GetProducts(null, "Electronics", 100);
+
+Console.WriteLine("###################################");
+//
