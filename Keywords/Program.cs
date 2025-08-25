9c3ebcd4724722daf1dab8316d8f60f2243bbe0e
Harry Keys <harry.keys40@gmail.com>
2025-08-25 21:41:52 +1000

    Updating readme, new keywords project

---
+++ Keywords/Program.cs
@@ -0,0 +1,12 @@
+﻿using Keywords;
+
+// See https://aka.ms/new-console-template for more information
+
+//Internal Keyword
+Console.WriteLine(Internal.x);
+
+//Unsafe Keyword
+
+//Using Keyword
+
+//try-catch-finally
