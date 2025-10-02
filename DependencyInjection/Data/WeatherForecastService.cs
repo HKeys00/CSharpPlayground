4aed7cef9eb736d1fd90480a408392c1aea49f81
Harry Keys <harry.keys40@gmail.com>
2025-10-03 07:05:42 +1000

    Walking through the different lifespans of service injected through dependency injection

---
+++ DependencyInjection/Data/WeatherForecastService.cs
@@ -0,0 +1,27 @@
+namespace DependencyInjection.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public int Count { get; set; }
+
+        public WeatherForecastService()
+        {
+            Count = 0;
+        }
+
+        public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+        {
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
