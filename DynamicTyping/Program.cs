5423c655629a156bb79a969a82c4b941435fba7d
Harry Keys <harry.keys40@gmail.com>
2025-10-03 08:05:05 +1000

    playing around with dynamic typing

---
+++ DynamicTyping/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DynamicTyping;
+
+dynamic obj = new SimpleDynamicExample();
+
+obj.WhosInParis("You know who");
+Console.WriteLine(obj.SomeonesInParis);
---
+++ DynamicTyping/SimpleDynamicExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicTyping
+{
+    internal class SimpleDynamicExample : DynamicObject
+    {
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object?[]? args, out object? result)
+        {
+            Console.WriteLine($"Invoked : {binder.Name}, {string.Join(", ", args)}");
+            result = null;
+            return true;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object? result)
+        {
+            result = $"Fetched : {binder.Name}";
+            return true;
+        }
+    }
+}
