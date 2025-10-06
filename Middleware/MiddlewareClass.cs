fd87aa14fec1999ef26dfe6a9a1e0884a9a94eca
Harry Keys <harry.keys40@gmail.com>
2025-10-07 07:42:10 +1100

    Added middleware project

---
+++ Middleware/MiddlewareClass.cs
@@ -0,0 +1,25 @@
+﻿namespace Middleware
+{
+    public class MiddlewareClass
+    {
+        private readonly RequestDelegate _next;
+        public MiddlewareClass(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("REQUEST GOES THROUGH HERE");
+            await _next(context);
+        }
+    }
+
+    public static class RequestMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareClass(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<MiddlewareClass>();
+        }
+    }
+}
