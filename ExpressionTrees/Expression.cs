45f18b0231f56a1f43466e2d0788d2c17f6fcaeb
Harry Keys <harry.keys40@gmail.com>
2025-10-07 07:42:37 +1100

    Added expression tree project

---
+++ ExpressionTrees/Expression.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionTrees
+{
+    public class Expressions
+    {
+        public static Expression<Func<T, bool>> BuildPredicate<T>(FilterRequest filter)
+        {
+            var parameter = Expression.Parameter(typeof(T), "x");
+            var property = Expression.Property(parameter, filter.Field);
+            var constant = Expression.Constant(Convert.ChangeType(filter.Value, property.Type));
+
+            Expression body = filter.Operator switch
+            {
+                "Equal" => Expression.Equal(property, constant),
+                "NotEqual" => Expression.NotEqual(property, constant),
+                "GreaterThan" => Expression.GreaterThan(property, constant),
+                "LessThan" => Expression.LessThan(property, constant),
+                "Contains" => Expression.Call(property, "Contains", null, constant),
+                        _ => throw new NotSupportedException($"Operator {filter.Operator} not supported")
+
+            };
+
+            return Expression.Lambda<Func<T, bool>>(body, parameter);
+        }
+    }
+
+    public class FilterRequest
+    {
+        public string Field { get; set; }
+        public string Operator { get; set; }
+        public object Value { get; set; }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Sex { get; set; } //0 Male 1 Female
+    }
+}
---
+++ ExpressionTrees/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExpressionTrees;
+
+Console.WriteLine("Hello, World!");
+
+var users = new List<User>
+{
+    new User { Age = 25, Name = "Alice" },
+    new User { Age = 40, Name = "Bob" },
+    new User { Age = 18, Name = "Charlie" }
+}.AsQueryable();
+
+var filter = new FilterRequest { Field = "Age", Operator = "GreaterThan", Value = 30 };
+
+// Build predicate dynamically
+var predicate = Expressions.BuildPredicate<User>(filter);
+
+// Apply to query
+var results = users.Where(predicate).ToList();
