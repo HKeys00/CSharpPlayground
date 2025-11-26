8eb2eb2e2ffe2a5a22a32e6ca00a7f304578d04c
Harry Keys <harry.keys40@gmail.com>
2025-11-27 08:49:46 +1100

    Messing around with async/await

---
+++ Async/Program.cs
@@ -0,0 +1,11 @@
+﻿using Async;
+
+Console.WriteLine("=== Starting ===");
+
+// Simulate two events triggering around the same time
+await SyncVsAsync.HandleRequestAsync("A").ConfigureAwait(false);
+await SyncVsAsync.HandleRequestAsync("B");
+
+//await Task.WhenAll(t1, t2);
+
+Console.WriteLine("=== Done ===");
---
+++ Async/SyncVsAsync.cs
@@ -0,0 +1,47 @@
+﻿namespace Async
+{
+    internal static class SyncVsAsync
+    {
+        public static bool Busy = false;
+
+        public static async Task<int> SynchronouslyCompletingTask(bool completeSync)
+        {
+            if (completeSync)
+            {
+                //throw new ArgumentNullException(nameof(completeSync));
+                return 0;
+            }
+
+            await Task.Delay(1000); //Perform work.
+            return 1;
+        }
+
+        public static async Task<int> AsyncCompletingTask()
+        {
+            await Task.Delay(1000);
+            return 0;
+        }
+
+        public static async Task HandleRequestAsync(string name)
+        {
+            Console.WriteLine($"{name}: Entering");
+
+            if (Busy)
+            {
+                Console.WriteLine($"{name}: ERROR — reentered while busy!");
+                return;
+            }
+
+            Busy = true;   // Expect Busy to remain true until AFTER await
+
+            Console.WriteLine($"{name}: await hit on {Environment.CurrentManagedThreadId}, returning and scheduling continuation");
+            await SynchronouslyCompletingTask(false);
+
+            Console.WriteLine($"{name}: Continuation run on {Environment.CurrentManagedThreadId}");
+
+            Busy = false;
+
+            Console.WriteLine($"{name}: Leaving");
+        }
+    }
+}
