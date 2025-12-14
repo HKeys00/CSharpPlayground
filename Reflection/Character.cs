3257abd567a6aaacd47e7620368d3853128d8e4c
Harry Keys <harry.keys40@gmail.com>
2025-12-15 08:26:21 +1100

    Studying relfection

--- Reflection/Character.cs
+++ Reflection/Character.cs
@@ -8,5 +8,24 @@ namespace Reflection
 {
     public class Character
     {
+        private int field1 = 0;
+        public int property1 { get; set; }
+
+
+        public Character()
+        {
+
+        }
+
+        public void PublicMethod()
+        {
+
+        }
+
+        private void PrivateMethod()
+        {
+
+        }
+
     }
 }
--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -26,3 +26,25 @@ foreach (Character character in characters)
         Console.WriteLine("Methods back stab not found");
     }
 }
+
+var tyoes = new Type[0];
+var type1 = typeof(Character);
+
+var m = Activator.CreateInstance(type1);
+var l = type1.GetConstructor(BindingFlags.Public | BindingFlags.Instance, null, CallingConventions.HasThis, tyoes, null)?.Invoke(null);
+
+var methods = type1.GetMethods(BindingFlags.Static | BindingFlags.NonPublic);
+var fields = type1.GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+var properties = type1.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+var field = fields[0];
+field.SetValue(m, 5);
+
+var property = properties[0];
+property.SetValue(l, 5);
+var t = property.GetValue(l);
+
+var type2 = characters[0].GetType();
+var type3 = Type.GetType("Character");
+
+Console.ReadLine();
