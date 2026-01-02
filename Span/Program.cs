d2789a1e5760b5fce77b97ed850fcac89f53110e
Harry Keys <harry.keys40@gmail.com>
2026-01-03 10:42:14 +1100

    Messing around with spans

---
+++ Span/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Span<MutableStruct> spanStructs = new MutableStruct[1];
+List<MutableStruct> listStructs = new List<MutableStruct>() { new MutableStruct() };
+
+spanStructs[0] = listStructs[0];
+listStructs[0].Value = 0;
+
+var arr = new byte[10];
+
+Span<byte> bytes = arr;
+bytes[0] = 10;
+
+Console.ReadLine();
+
+struct MutableStruct
+{
+    public int Value;
+}
