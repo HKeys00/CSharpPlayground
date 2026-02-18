c94aaeeac9a4ff24a1f7006b04e4b6269a606434
Harry Keys <harry.keys40@gmail.com>
2026-02-19 08:28:39 +1100

    Trying to solidify mental models of iterators

---
+++ Linq/Methods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public static class Methods
+    {
+        public static IEnumerable<int> WhereEven(this IEnumerable<int> x)
+        {
+            foreach(var item in x)
+            {
+                if (item % 2 == 0)
+                    yield return item;
+            }
+        }
+
+        public static IEnumerable<IReadOnlyList<T>> Batch<T>(
+        this IEnumerable<T> source,
+        int size)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (size <= 0) throw new ArgumentOutOfRangeException(nameof(size));
+
+            List<T>? bucket = null;
+
+            foreach (var item in source)
+            {
+                bucket ??= new List<T>(size);
+                bucket.Add(item);
+
+                if (bucket.Count == size)
+                {
+                    yield return bucket;
+                    bucket = null; // start next batch
+                }
+            }
+
+            if (bucket != null && bucket.Count > 0)
+                yield return bucket;
+        }
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Linq;
+
+Console.WriteLine("Hello, World!");
+
+
+List<int> numbers = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+var num = numbers.Where(x => x < 8).WhereEven();
+
+foreach( int number in num)
+{
+    Console.WriteLine(number);
+    num = Enumerable.Range(0, 10);
+}
+
+var batches =
+    Enumerable.Range(1, 10)
+              .Batch(3);
+
+foreach (var batch in batches)
+{
+    Console.WriteLine(string.Join(",", batch));
+}
